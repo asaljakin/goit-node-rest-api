d7bc33faf4b9dbcb837806c4cec1dcbd2bd3bfc0
Andrii <asaljakin@gmail.com>
2024-05-30 20:48:50 +0300

    Update authSchemas.js

--- schemas/authSchemas.js
+++ schemas/authSchemas.js
@@ -4,7 +4,7 @@ import { SUBSCRIPTIONS, emailRegexp } from "../constants/contacts-constants.js";
 export const userRegisterSchema = Joi.object({
   password: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
-  subscription: Joi.string(),
+  subscription: Joi.string().valid(...SUBSCRIPTIONS),
   token: Joi.string(),
 });
 
